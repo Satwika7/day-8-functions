2c56db999eab75b1230c51626212bf94cf918c50
Satwika7 <113924355+Satwika7@users.noreply.github.com>
2022-09-26 06:22:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+  print("hi\nhello\nbye")
+greet()
+def fun(name,loc):
+  print(f"hi {name},{loc} is a great place")
+#positional arguments
+fun("sam","kashmir")
+#keyword arguments
+fun(loc="kashmir",name="sam")
+
